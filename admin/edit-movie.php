5326f7046d5966bf8cecb3ed3b3ddbd9d672f7b6
sulo <sulo@gmail.com>
2023-03-08 14:42:57 -0600

    edited file

--- admin/edit-movie.php
+++ admin/edit-movie.php
@@ -100,7 +100,6 @@ if (($_SERVER['REQUEST_METHOD'] == 'POST')) {
                                             <span>Movies</span>
                                             <i class="fas fa-chevron-right"></i>
                                         </a>
-
                                     </li>
                                     <li>
                                         <a>
